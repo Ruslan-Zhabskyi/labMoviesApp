595d0e29c9c98c5d427807497142983ef8be7543
ruslanZhabskyi <ru.zhabskyi@gmail.com>
2024-06-17 19:50:57 +0100

    Added Upcoming movies page.

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,15 @@ import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavouriteMoviesPage from "./pages/favouriteMoviesPage"; // NEW
 import MovieReviewPage from "./pages/movieReviewPage";
-import SiteHeader from './components/siteHeader'
+import SiteHeader from './components/siteHeader';
+import UpcomingMoviesPage from "./pages/upcomingMoviesPage"
 
 const App = () => {
     return (
         <BrowserRouter>
             <SiteHeader />      {/* New Header  */}
             <Routes>
+                <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} />
                 <Route path="/reviews/:id" element={<MovieReviewPage/>} />
                 <Route path="/movies/favourites" element={<FavouriteMoviesPage />} />
                 <Route path="/movies/:id" element={<MoviePage />} />
